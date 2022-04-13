bf71d0406b227fa403ae7a75f4c80598c9ecb168
Paola Ayala <paolaaz235@gmail.com>
2022-04-12 22:44:01 -0400

    Fix DOM Methods

--- main.js
+++ main.js
@@ -8,60 +8,62 @@ Start at 1:10:29
  */
 
 // Single element
-// console.log(document.getElementById('my-form'));
-// console.log(document.querySelector('h1));
+//console.log(document.getElementById('my-form'));
+//console.log(document.querySelector('h1'));
 
 // Multiple element
-// console.log(document.querySelectorAll('.item'));
-// console.log(document.getElementsByClassName('item'));
-// console.log(document.getElementsByTagName('li'));
+//console.log(document.querySelectorAll('.item'));
+//console.log(document.getElementsByClassName('item'));
+//console.log(document.getElementsByTagName('li'));
 
-// const items = document.querySelectorAll('.item');
+//const items = document.querySelectorAll('.item');
 
-// items.forEach((item) => console.log(item));
+//items.forEach((item) => console.log(item));
 
-// const ul = document.querySelector('.items');
+//const ul = document.querySelector('.items');
 
-// ul.remove();
-// ul.lastElementChild.remove();
-// ul.firstElementChild.textContent = 'Hello';
-// ul.children[1].innerText = 'Sophie';
-// ul.lastElementChild.innterHTML = '<h1>Hello</h1>';
+//ul.remove();
+//ul.lastElementChild.remove();
+//ul.firstElementChild.textContent = 'Hello';
+//ul.children[1].innerText = 'Brad';
+//ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
 
-// const btn = document.querySelector('.btn');
-// btn.style.background = 'red';
+//const btn = document.querySelector('.btn');
+//btn.style.background = 'red';
 
 /* btn.addEventListener('mouseout', (e) => {
-    e.preventDefault();
-    document.querySelector('#my-form').style.background = '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
-    document.querySelector('.items').lastElementChild.innerHTML =
-        '<h1>Hello</h1>;
+  e.preventDefault();
+  document.querySelector('#my-form').style.background = '#ccc';
+  document.querySelector('body').classList.add('bg-dark');
+  document.querySelector('.items').lastElementChild.innerHTML =
+    '<h1>Hello</h1>';
 }); */
 
-// const myForm = document.querySelector('#my-form');
-// const nameInput = document.querySelector('#name');
-// const emailInput = document.querySelector('#email');
-// const msg = document.querySelector('.msg');
-// const userList = document.querySelector('#users');
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
 
-// myForm.addEventListener('submit', onSubmit);
+myForm.addEventListener('submit', onSubmit);
 
-// function onSubmit(e) {
-//  e.preventDefault();
+function onSubmit(e) {
+  e.preventDefault();
 
-//  if (nameInput.value === '' || emailInput.value === '') {
-//     msg.classList.add('error');
-//     msg.innerHTML = 'Please enter all fields';
+  if (nameInput.value === '' || emailInput.value === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
 
-//     setTimeout(() => msg.remove(), 3000);
-//   } else {
-//     const li = document.createElement('li');
-//     li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
-//     userList.appendChild(li);
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const li = document.createElement('li');
+    li.appendChild(
+      document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
+    );
+    userList.appendChild(li);
 
-//Clear fields
-//     nameInput.value = '';
-//     emailInput.value = '';
-//   }
-// }
+    //Clear fields
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}
